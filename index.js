98c365bf326ff920a3c9bf2b05ac47d2b859ae88
Anthony David Cobb <anthonydcobb@gmail.com>
2020-06-01 16:50:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if (cardNumber === undefined) {
-    return "Sorry, we don't have a credit card of file for you."
+    return "Sorry, we don't have a credit card on file for you."
   } else {
     finalTotal = total();
     let saleStr = `Your total cost is ${finalTotal}, which will be charged to the card ${cardNumber}. `
